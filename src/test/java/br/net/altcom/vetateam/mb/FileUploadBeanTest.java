607ab118149c92b34ba3a8160091877be1247f8f
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-05-18 12:53:15 -0300

    Adicionando a classe de teste para FileUploadBean.java

---
+++ src/test/java/br/net/altcom/vetateam/mb/FileUploadBeanTest.java
@@ -0,0 +1,14 @@
+package br.net.altcom.vetateam.mb;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileUploadBeanTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
