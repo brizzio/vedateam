073f8e64d62949945d07c254d226bc190837fd42
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-05-18 17:51:37 -0300

    Alterando LancamentoDao

--- src/main/java/br/net/altcom/vetateam/dao/LancamentoDao.java
+++ src/main/java/br/net/altcom/vetateam/dao/LancamentoDao.java
@@ -3,14 +3,13 @@ package br.net.altcom.vetateam.dao;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceUnit;
 
 import br.net.altcom.vetateam.modelo.Lancamento;
 
 @Named
 public class LancamentoDao {
 
-	@PersistenceUnit(unitName="fly")
+	@PersistenceContext(unitName="fly")
 	private EntityManager manager;
 
 	public void adiciona(Lancamento lancamento) {
