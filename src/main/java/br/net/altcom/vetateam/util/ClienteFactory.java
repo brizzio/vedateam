f01cbdde326b4e4759516d1bf5bf1115f020c22c
Francisco <franciscocostaalves2@gmail.com>
2017-05-22 17:02:27 -0300

    Adicionando uma Factory para cliente

---
+++ src/main/java/br/net/altcom/vetateam/util/ClienteFactory.java
@@ -0,0 +1,15 @@
+package br.net.altcom.vetateam.util;
+
+import java.util.Map;
+
+import br.net.altcom.vetateam.modelo.Cliente;
+
+public class ClienteFactory {
+
+	public Cliente getCliente(Map<String, String> rowMap){
+		
+		Cliente cliente = new Cliente();
+		cliente.setNome(rowMap.get(LancamentoHeader.CLIENTE.getHeaderName()));
+		return cliente;
+	}
+}
