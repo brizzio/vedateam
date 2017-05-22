00c4164aeb57cadd3396fe42e1e0be8694841f26
Francisco <franciscocostaalves2@gmail.com>
2017-05-22 16:57:32 -0300

    Adicionando o método 'buscaPeloNome'

--- src/main/java/br/net/altcom/vetateam/dao/ClienteDao.java
+++ src/main/java/br/net/altcom/vetateam/dao/ClienteDao.java
@@ -3,6 +3,7 @@ package br.net.altcom.vetateam.dao;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import br.net.altcom.vetateam.modelo.Cliente;
 
@@ -14,5 +15,15 @@ public class ClienteDao {
 
 	public void adiciona(Cliente cliente) {
 		manager.persist(cliente);
+	}
+	
+	public Cliente buscaPeloNome(Cliente cliente) {
+		String jpql = "Select c from Cliente c where c.nome like :nome";
+		TypedQuery<Cliente> query = manager.createQuery(jpql, Cliente.class);
+		try {
+			return query.setParameter("nome", cliente.getNome()).getSingleResult();
+		} catch (javax.persistence.NoResultException e) {
+			return null;
+		}
 	}
 }
