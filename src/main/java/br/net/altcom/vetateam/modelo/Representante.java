e8851c25c08972a682d89870bac1b2e8125468dd
Francisco <franciscocostaalves2@gmail.com>
2017-05-22 11:40:07 -0300

    Adicionando a classe representante

---
+++ src/main/java/br/net/altcom/vetateam/modelo/Representante.java
@@ -0,0 +1,32 @@
+package br.net.altcom.vetateam.modelo;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Representante {
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	@OneToMany(mappedBy="representante")
+	private List<Cliente> cliente;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void adicionaCliente(Cliente cliente){
+		this.cliente.add(cliente);
+	}
+}
