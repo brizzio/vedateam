ce4dfe5672c5909a04bb444ec9d6bd37cb496fce
Francisco <franciscocostaalves2@gmail.com>
2017-05-22 17:21:52 -0300

    Alterando Representante, id não e mais autoIncrement e preciso dar um id para persistir ao banco, e um set e get para o id

--- src/main/java/br/net/altcom/vetateam/modelo/Representante.java
+++ src/main/java/br/net/altcom/vetateam/modelo/Representante.java
@@ -3,8 +3,6 @@ package br.net.altcom.vetateam.modelo;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
@@ -12,13 +10,20 @@ import javax.persistence.OneToMany;
 public class Representante {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
 	@OneToMany(mappedBy = "representante")
 	private List<Cliente> cliente;
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public List<Cliente> getCliente() {
+		return cliente;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
