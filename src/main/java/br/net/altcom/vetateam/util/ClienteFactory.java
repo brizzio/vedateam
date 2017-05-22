470b0ca4dcaf79dd4210a8938afeb6d32b129a49
Francisco <franciscocostaalves2@gmail.com>
2017-05-22 17:24:11 -0300

    Alterando o método getCliente, o representante e adicionado no cliente

--- src/main/java/br/net/altcom/vetateam/util/ClienteFactory.java
+++ src/main/java/br/net/altcom/vetateam/util/ClienteFactory.java
@@ -3,13 +3,18 @@ package br.net.altcom.vetateam.util;
 import java.util.Map;
 
 import br.net.altcom.vetateam.modelo.Cliente;
+import br.net.altcom.vetateam.modelo.Representante;
 
 public class ClienteFactory {
 
 	public Cliente getCliente(Map<String, String> rowMap){
 		
 		Cliente cliente = new Cliente();
+		Representante representante = new RepresentanteFactory().getRepresentante(rowMap);
+		
 		cliente.setNome(rowMap.get(LancamentoHeader.CLIENTE.getHeaderName()));
+		cliente.setRepresentante(representante);
+		
 		return cliente;
 	}
 }
