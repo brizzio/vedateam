e2ad3b4793951004ab487428448908ba7d06f56e
Francisco <franciscocostaalves2@gmail.com>
2017-05-24 16:53:43 -0300

    Adicionando método login no bean LoginBean

--- src/main/java/br/net/altcom/vetateam/mb/LoginBean.java
+++ src/main/java/br/net/altcom/vetateam/mb/LoginBean.java
@@ -1,11 +1,25 @@
 package br.net.altcom.vetateam.mb;
 
 import javax.enterprise.inject.Model;
+import javax.inject.Inject;
 
+import br.net.altcom.vetateam.dao.UsuarioDao;
 import br.net.altcom.vetateam.modelo.Usuario;
 
 @Model
 public class LoginBean {
 
 	private Usuario usuario = new Usuario();
+	@Inject
+	private UsuarioDao usuarioDao;
+	public void login(){
+		if (!usuarioDao.isExiste(usuario))
+			return;
+		
+		Usuario usuarioBanco = usuarioDao.buscaPorEmail(usuario);
+		
+		if (usuarioBanco.getSenha().equals(usuario.getSenha())) {
+			System.out.println("Fez login");
+		}
+	}
 }
