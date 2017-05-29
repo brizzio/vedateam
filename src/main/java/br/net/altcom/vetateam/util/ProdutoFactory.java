2dc98e6492d9f50ea3f71a49f54f16d2527ab67a
Francisco <franciscocostaalves2@gmail.com>
2017-05-29 01:41:02 -0300

    Refatorando as classes ClienteFactory, RepresentanteFactory, ProdutoFactory

--- src/main/java/br/net/altcom/vetateam/util/ProdutoFactory.java
+++ src/main/java/br/net/altcom/vetateam/util/ProdutoFactory.java
@@ -9,9 +9,7 @@ public class ProdutoFactory {
 	public Produto getProduto(Map<String, String> rowMap){
 		Produto produto = new Produto();
 		
-		String idString = rowMap.get(LancamentoHeader.ID_ITEM.getHeaderName());
-		produto.setId(new Integer(idString));
-		
+		produto.setIdProduto(rowMap.get(LancamentoHeader.ID_ITEM.getHeaderName()));
 		produto.setProduto(rowMap.get(LancamentoHeader.PRODUTO.getHeaderName()));
 		produto.setItem(rowMap.get(LancamentoHeader.ITEM.getHeaderName()));
 		produto.setFamilia(rowMap.get(LancamentoHeader.FAMILIA.getHeaderName()));
--- src/main/java/br/net/altcom/vetateam/util/RepresentanteFactory.java
+++ src/main/java/br/net/altcom/vetateam/util/RepresentanteFactory.java
@@ -8,12 +8,22 @@ public class RepresentanteFactory {
 
 	public Representante getRepresentante(Map<String, String> rowMap) {
 		Representante representante = new Representante();
-		
+
 		String idString = rowMap.get(LancamentoHeader.ID_REPRESENTANTE.getHeaderName());
-		representante.setId(new Integer(idString));
-		
+
+		if (idString.equals("-")) {
+			representante.setId(0);
+		}else{			
+			try {
+				representante.setId(new Integer(idString));
+			} catch (NumberFormatException e) {
+				System.out.println("Erro ao converter String para id: " + idString + " do representante: "
+						+ rowMap.get(LancamentoHeader.REPRESENTANTE.getHeaderName()));
+			}
+		}
+
 		representante.setNome(rowMap.get(LancamentoHeader.REPRESENTANTE.getHeaderName()));
-		
+
 		return representante;
 	}
 
