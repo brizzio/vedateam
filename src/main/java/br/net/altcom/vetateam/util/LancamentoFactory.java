42679d50b881562f22bb4cf6785e7deb8dcab9fd
Francisco <franciscocostaalves2@gmail.com>
2017-05-29 01:41:30 -0300

    Refatorando as classes LancamentoFactory

--- src/main/java/br/net/altcom/vetateam/util/LancamentoFactory.java
+++ src/main/java/br/net/altcom/vetateam/util/LancamentoFactory.java
@@ -4,22 +4,14 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Map;
 
-import br.net.altcom.vetateam.modelo.Cliente;
 import br.net.altcom.vetateam.modelo.Lancamento;
-import br.net.altcom.vetateam.modelo.Produto;
 
 public class LancamentoFactory {
 	
 	public Lancamento getLancamento(Map<String, String> rowMap){
 		
 		Lancamento lancamento = new Lancamento();
-		
-		Produto produto = new ProdutoFactory().getProduto(rowMap);
-		lancamento.setProduto(produto);
-		
-		Cliente cliente = new ClienteFactory().getCliente(rowMap);
-		lancamento.setCliente(cliente);
-		
+						
 		BigDecimal faturamento = new BigDecimal(rowMap.get(LancamentoHeader.FATURA.getHeaderName()));
 		lancamento.setFaturamento(faturamento);
 		
