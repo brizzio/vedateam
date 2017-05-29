bee5b4d445b56cdeed4c46b66ea8b8e96d5ec501
Francisco <franciscocostaalves2@gmail.com>
2017-05-29 01:37:03 -0300

    refatorando a classe FileUploadBean

--- src/main/java/br/net/altcom/vetateam/mb/FileUploadBean.java
+++ src/main/java/br/net/altcom/vetateam/mb/FileUploadBean.java
@@ -1,15 +1,20 @@
 package br.net.altcom.vetateam.mb;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
+import br.net.altcom.vetateam.util.ExcelFactory;
 import br.net.altcom.vetateam.util.ExcelLancamentoProcessor;
 
 @Named
@@ -22,24 +27,36 @@ public class FileUploadBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private UploadedFile file;
-	
+
 	@Inject
 	private ExcelLancamentoProcessor processor;
-	
-	public void upload() throws IOException{
+
+	private List<String> sheetsName = new ArrayList<>();
+
+	public List<String> getSheetsName() {
+		return sheetsName;
+	}
+
+	public void processarExcel(String sheetName) throws IOException {
 		if (file != null) {
-			InputStream inputstream = file.getInputstream();
-			processor.setFile(inputstream);
-			new Thread(processor).start();
+			processor.setSheetName(sheetName);
+			try (InputStream inputStream = new ByteArrayInputStream(file.getContents())){
+				processor.setFile(inputStream);
+				new Thread(processor).start();
+				sheetsName.remove(sheetName);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
-	public UploadedFile getFile() {
-		return file;
+	public void handleFileUpload(FileUploadEvent event) {
+		file = event.getFile();
+		try (InputStream inputStream = new ByteArrayInputStream(file.getContents());
+				ExcelFactory excelFactory = new ExcelFactory(inputStream)){
+			sheetsName = excelFactory.getSheetName();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
-	public void setFile(UploadedFile file) {
-		this.file = file;
-	}
-
 }
